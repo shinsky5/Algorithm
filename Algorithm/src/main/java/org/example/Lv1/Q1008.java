a85c517da32c98211edeaa9cc0ca3c1c9e5df70a
shinsky5 <ohhanel2219@naver.com>
2025-02-10 09:03:36 +0900

    First Push

---
+++ Algorithm/src/main/java/org/example/Lv1/Q1008.java
@@ -0,0 +1,15 @@
+package org.example.Lv1;
+
+import java.util.Scanner;
+
+public class Q1008 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+
+        Double a = sc.nextDouble();
+        Double b = sc.nextDouble();
+
+        System.out.println(a/b);
+    }
+}
---
+++ Algorithm/src/main/java/org/example/Lv2/Q2884.java
@@ -0,0 +1,26 @@
+package org.example.Lv2;
+
+import java.util.Scanner;
+
+public class Q2884 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+
+        if(b>=45){
+            System.out.print(a+" ");
+            System.out.print(b-45);
+        }else{
+            if(a>0){
+                System.out.print(a-1+" ");
+                System.out.print(60-(45-b));
+            }else if(a==0){
+                System.out.print("23 ");
+                System.out.print(60-(45-b));
+            }
+        }
+
+    }
+}
